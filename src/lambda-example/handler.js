ad8707efe22da9224867f2dca8dd6f635e0160b2
daniel corcoran <danielc.92@outlook.com>
2022-07-27 21:08:03 +1000

    eslint run

--- src/lambda-example/handler.js
+++ src/lambda-example/handler.js
@@ -1,28 +1,28 @@
-"use strict";
-var figlet = require("figlet");
+'use strict'
+const figlet = require('figlet')
 
 module.exports.hello = async (event) => {
-  console.log("[LOG] Hello from daniels-first-lambda")
-  console.warn("[WARN] Test warning from daniels-first-lambda!");
-  figlet("THIS IS A SIMPLE LAMBDA!", (err, result) => {
+  console.log('[LOG] Hello from daniels-first-lambda')
+  console.warn('[WARN] Test warning from daniels-first-lambda!')
+  figlet('THIS IS A SIMPLE LAMBDA!', (err, result) => {
     if (!err) {
-      console.log(result);
+      console.log(result)
     } else {
-      console.error("Failed to use figlet.");
+      console.error('Failed to use figlet.')
     }
-  });
+  })
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Looks like the lambda is working...",
-        input: event,
+        message: 'Looks like the lambda is working...',
+        input: event
       },
       null,
       2
-    ),
-  };
+    )
+  }
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+}
--- src/stream-lambda/handler.js
+++ src/stream-lambda/handler.js
@@ -1,9 +1,9 @@
-module.exports.streamer = function(event, context, callback) {
-    console.log("STREAM LAMBDA: ", JSON.stringify(event, null, 2));
-    event.Records.forEach(function(record) {
-        console.log('STREAM LAMBDA RECORD LOG: ', record.eventID);
-        console.log('STREAM LAMBDA RECORD LOG: ', record.eventName);
-        console.log('STREAM LAMBDA RECORD LOG: ', record);
-    });
-    callback(null, "message");
-};
+module.exports.streamer = function (event, context, callback) {
+  console.log('STREAM LAMBDA: ', JSON.stringify(event, null, 2))
+  event.Records.forEach(function (record) {
+    console.log('STREAM LAMBDA RECORD LOG: ', record.eventID)
+    console.log('STREAM LAMBDA RECORD LOG: ', record.eventName)
+    console.log('STREAM LAMBDA RECORD LOG: ', record)
+  })
+  callback(null, 'message')
+}
